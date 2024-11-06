d355a9ea5946bca766c49fe9513addf8f8d8a3ff
karthic2914 <karthic2914@gmail.com>
2024-11-06 13:52:18 +0100

    Initial commit

---
+++ src/components/Styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+export const colors = {
+    primary:"#fff",
+    theme: "#BE185d",
+    light1:"#F3F4F6",
+    light2:"#E5E7E8",
+    dark1:"#1f2937",
+    dark2:"#4b5563",
+    dark3: "#9ca3af",
+    red:"#dc2626"
+}
